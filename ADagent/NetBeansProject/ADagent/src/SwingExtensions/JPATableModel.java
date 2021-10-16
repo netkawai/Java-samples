bf9496cd9292ccd467798902dfbc13158eaa173a
netkawai <netkawai@users.noreply.github.com>
2021-10-15 23:19:59 -0400

    It is better to make Generic one with reflection

--- ADagent/NetBeansProject/ADagent/src/SwingExtensions/JPATableModel.java
+++ ADagent/NetBeansProject/ADagent/src/SwingExtensions/JPATableModel.java
@@ -2,12 +2,12 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package adagent;
+package SwingExtensions;
 
 /**
  *
  * @author netka
  */
-public class EmployeeTableModel {
+public class JPATableModel {
     
 }
